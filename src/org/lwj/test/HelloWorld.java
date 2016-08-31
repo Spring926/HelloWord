c897c7cc1bb58a448b9607fc2e1dde124469fe4c
Spring926 <lwj18829298069@163.com>
2016-08-31 21:48:15 +0800

    test git commit and push

---
+++ src/org/lwj/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.lwj.test;
+
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 * test git push
+	 */
+     public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
